31728595cc68805936e8cf1fb0ad8c16ebe7242e
George Gordon <gsdgordon@gmail.com>
2020-09-30 14:41:00 +0100

    Added matlab code for visulasing and fitting data

---
+++ fitAerosolDist.m
@@ -0,0 +1,79 @@
+% Function to fit aerosol distirbution
+
+function [A, mu, sigma] = fitAerosolDist(diameters, densities, varargin)
+
+    p = inputParser;
+    addOptional(p,'initPop',[]);
+    addOptional(p,'fitType','counts');
+    parse(p,varargin{:});
+
+    initPop_d = p.Results.initPop;
+    initPop_r = log(exp(initPop_d)/2);
+    
+    popSize = 50;
+    if (size(initPop_r,1) > popSize/2)
+        initPop_r = initPop_r(end-popSize/2:end,:);
+    end
+    
+    
+    fitType = p.Results.fitType;
+
+    % Using models based around radii fitting
+    % (http://eodg.atm.ox.ac.uk/user/grainger/research/aerosols.pdf)
+    radii = diameters/2;
+    diameters_av = (diameters(1:6)+diameters(2:7))/2; % Mean diameter in each bin
+    binSizes = (diameters(2:end) - diameters(1:end-1));
+
+
+    % set up a function to numerically integrate over the bin size range
+    nIntSteps = 4;
+    
+    if strcmpi(fitType,'counts')
+        intFun = @(y) trapz(lognpdf(linspace(y(1),y(2),nIntSteps),y(3),y(4)))*(y(2)-y(1))/(nIntSteps-1); % For particle count
+    elseif strcmpi(fitType,'volume')
+        intFun = @(y) trapz(1./linspace(y(1),y(2),nIntSteps).*lognpdf(linspace(y(1),y(2),nIntSteps),y(3),y(4)))*(y(2)-y(1))/(nIntSteps-1); % For total volume
+    end
+    
+    prior = @(x) 1; % Uninformative prior
+    
+    objFun = @(x) -1*(densities(1)*log(intFun([radii(1),radii(2),x(1),x(2)]))...
+                    + densities(2)*log(intFun([radii(2),radii(3),x(1),x(2)]))...
+                    + densities(3)*log(intFun([radii(3),radii(4),x(1),x(2)]))...
+                    + densities(4)*log(intFun([radii(4),radii(5),x(1),x(2)]))...
+                    + densities(5)*log(intFun([radii(5),radii(6),x(1),x(2)]))...
+                    + densities(6)*log(intFun([radii(6),radii(7),x(1),x(2)]))...
+                    + log(prior(x)));
+                
+
+
+    % First run a genetic search to avoid local minima        
+    options_ga = optimoptions('ga', 'TolFun',1e-14, 'InitialPopulationMatrix',initPop_r, 'MaxGenerations',1000, 'PopulationSize', popSize, 'Display', 'off');
+    bestVal = ga(objFun,2,[],[],[],[],[],[],[],options_ga);
+            
+   
+    % Refine using a local search
+    options_fminunc = optimoptions('fminunc','MaxFunctionEvaluations',1e4, 'StepTolerance',1e-10, 'OptimalityTolerance',1e-12, 'Display', 'off');
+    bestVal = fminunc(objFun,bestVal,options_fminunc);
+    
+   
+    % Convert back to diameters
+    A = sum(densities .* binSizes);
+    mu_r = bestVal(1);
+    mu = log(exp(mu_r)*2);
+    sigma_r = bestVal(2);
+    sigma = log(exp(sigma_r)*2);
+    
+    normConst = logncdf(diameters(7),mu,sigma) - logncdf(diameters(1),mu,sigma);
+
+    plotFit = true;
+  
+    if plotFit
+        plot(log(diameters_av),densities/A*normConst);
+        hold on;
+        plot(log(diameters_av),(logncdf(diameters(2:7),mu,sigma) - logncdf(diameters(1:6),mu,sigma))./binSizes,'r-');
+        hold off;
+        pause(0.01)
+    end
+
+
+end
---
+++ visualiseAerotrakData.m
@@ -0,0 +1,225 @@
+% File showing example of how to process/plot data from AeroTrak, fit a
+% lognormal distribution and creat a video
+% Author: George Gordon
+% Date: 30/09/2020
+
+
+% Clear any old data
+clc;
+clear variables;
+close all;
+
+% Load data
+T = readtable('20200915_aerotrak.xlsx');
+
+opTime = T.DateAndTime;
+location = T.Location;
+sampleTime = T.SampleTime;
+avSampleTime = mode(sampleTime); % assumes sample time is not changed during operation, but excludes partial samples
+
+
+% Sync times from different clocks as per the video
+obsCamTime_endo = datetime(2020,9,15,14,25,59);
+endoscopeTime = datetime(2020,9,15,14,41,08);
+
+obsCamTime_aerotrak = datetime(2020,9,15,14,26,10);
+aerotrakTime = datetime(2020,9,15,14,37,28) + seconds(avSampleTime); %Aerotrak time is the time at the start of the sample
+
+aeroOffsetTime = aerotrakTime - obsCamTime_aerotrak;
+endoOffsetTime = endoscopeTime - obsCamTime_endo;
+
+opTime = opTime - aeroOffsetTime;
+
+startTime = datetime(2020,9,15,14,25,18);
+endTime = datetime(2020,9,15,16,26,35);
+
+tValid = isbetween(opTime,startTime,endTime);
+tValid = tValid & strcmpi(location,'Location01');
+
+T = T(tValid,:);
+T = T(1:end-1,:); % remove last count as it is likely partial
+
+opTime2 = T.DateAndTime - aeroOffsetTime;
+airVol = T.Volume_L_;
+
+% Now get the particle 'diameters' representing the edges of the counting
+% bins
+maxDiameter = 25; % in microns - this is from the spec sheet
+diameters = [T.Ch1Size__m_(1),T.Ch2Size__m_(1),T.Ch3Size__m_(1),T.Ch4Size__m_(1),T.Ch5Size__m_(1),T.Ch6Size__m_(1), maxDiameter];
+diameters_av = (diameters(1:6)+diameters(2:7))/2; % Mean diameter in each bin
+
+
+% The actual counts in each bin
+data = [T.Ch1Diff___,T.Ch2Diff___,T.Ch3Diff___,T.Ch4Diff___,T.Ch5Diff___,T.Ch6Diff___];
+data = data./repmat(airVol,1,size(data,2)) * 1000; %Because volume is liters so times by 1000 to get to m^3
+
+
+% Convert counts to volumes
+vols = 4/3*pi*(diameters_av/2).^3 * (1e-6)^3;
+
+bin_sizes = diameters(2:7) - diameters(1:6);
+
+data_v = data .* repmat(vols,size(data,1),1);
+
+% Densities so that a probability density approach can be used
+data_v_density = data_v ./ repmat(bin_sizes,size(data,1),1);
+data_density = data ./ repmat(bin_sizes,size(data,1),1);
+
+initPop = [];
+
+%Try to fit lognormal distribution
+for k=1:size(data,1)
+    
+    densities = data_density(k,:);
+    densities_v = data_v_density(k,:);
+    
+    if any(isnan(densities))
+        A(k) = NaN;
+        mu(k) = NaN;
+        sigma(k) = NaN;
+    else
+        if k>1
+            initPop = [mu(1:k-1)',sigma(1:k-1)']; % Avoid nans!
+            validRows = ~isnan(initPop(:,1)) & ~isnan(initPop(:,2));
+            initPop = initPop(validRows,:);
+        end
+        
+        %[A_t, mu_t, sigma_t] = fitAerosolDist(diameters, densities,'fitType','counts','initPop',initPop);
+        [A_t, mu_t, sigma_t] = fitAerosolDist(diameters, densities_v,'fitType','volume','initPop',initPop);
+        
+        A(k) = A_t;
+        mu(k) = mu_t;
+        sigma(k) = sigma_t;
+        
+
+    
+        if mod(k,10) == 0
+            disp(['Done ', num2str(k), '/', num2str(size(data,1))]);
+        end
+
+    end
+
+end
+
+subplot(3,1,1)
+plot(opTime2, A);
+title('Relative aerosol volume');
+
+subplot(3,1,2);
+plot(opTime2, exp(mu));
+title('\mu (mean aerosol diameter in microns)');
+
+subplot(3,1,3);
+plot(opTime2, sigma);
+title('\sigma (standard devation of particle diameters in microns)');
+
+
+data_tot = sum(data_v,2);
+
+nSizes = size(data,2);
+tColor = lines(nSizes);
+
+allFig = figure;
+sumFig = figure;
+
+figure(allFig);
+for k=1:nSizes+1
+    
+    if (k <= nSizes)
+        subplot(nSizes,1,k);
+        plot(opTime2,data(:,k),'Color',tColor(k,:));
+        title(['Diameter: ', num2str(diameters(k)), '\mum']);
+        ylabel('#/m^3');
+        xlabel('time')
+       
+        if k==1
+            %yline(10200,'k:', 'ISO5')
+            yline(102000,'k:', 'ISO6')
+        end
+        
+        if k==2
+            %yline(3520,'k:', 'ISO5')
+            yline(35200,'k:', 'ISO6')
+            yline(352000,'k:', 'ISO7')
+        end
+        
+        if k==3
+            %yline(832,'k:', 'ISO5')
+            %yline(8320,'k:', 'ISO6')
+            yline(83200,'k:', 'ISO7')
+        end
+        
+        if k==5
+            %yline(29,'k:', 'ISO5')
+            %yline(293,'k:', 'ISO6')
+            yline(2930,'k:', 'ISO7')
+        end
+        
+        ylim_array(k,1:2) = ylim;
+    else
+        figure(sumFig)
+        plot(opTime2,data_tot,'Color','k');
+    end
+    
+    %xline(datetime(2020,8,7,14,33,00),'r--','attached to bed');
+
+
+end
+
+
+% Make a video suitable for overlaying on clinical videos
+makeVideo = true;
+
+if makeVideo
+    fps = 30;
+    window = 60; % in seconds
+
+    d = seconds(endTime - startTime);
+    nFrames = d*fps;
+
+    v = VideoWriter('test', 'FrameRate', fps);
+    open(v);
+    
+    for frameIdx = 1:nFrames
+        
+        currentStart = startTime + duration(0,0,frameIdx/fps - window);
+        currentEnd = currentStart + duration(0,0,window);
+    
+        for k=1:nSizes
+
+            subplot(nSizes,1,k);
+            if (frameIdx == 1)
+                
+                plot(opTime2,data(:,k),'Color',tColor(k,:));
+                %title(['Diameter: ', num2str(diameters(k)), '\mum']);
+                ylabel('v/m^3');
+                %xlabel("time")
+                
+                set(gcf,'Position',[100,100,200,600]);
+            end
+            axis tight;
+            ylim(ylim_array(k,1:2));
+            xlim([currentStart,currentEnd]);
+            
+            ax = gca;
+            set(ax(1),'XTickLabel','')
+
+        end
+        
+        frame = getframe(gcf);
+        writeVideo(v,frame);
+        
+
+        
+        if mod(frameIdx,fps*60) == 0
+            disp(['Frame ', num2str(frameIdx)]);
+            pause(1/fps);
+        end
+        
+    end
+    
+    close(v);
+
+    
+end
+
